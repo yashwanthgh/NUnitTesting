a94490e07fcd230af7102fc572a85dd1fca9f515
yashwanthgh <yugnasura@gmail.com>
2024-04-04 08:59:14 +0530

    Methods that will be tested are written

---
+++ OddEven.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class OddEven
+    {
+         int Number;
+
+        public OddEven(int n)
+        {
+            Number = n;
+        }
+        public string IsEven()
+        {
+            if(Number % 2 == 0)
+            {
+                return "Even";
+            } else
+            {
+                return "Odd";
+            }
+        }
+    }
+}
